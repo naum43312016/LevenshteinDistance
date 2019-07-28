0d46a73f8116cff0be690142ba24ae154c8bfedd
naum <naum43312016@gmail.com>
2019-07-28 13:42:49 +0300

    Initial commit

---
+++ src/com/asafov/naum/LevenshteinDistanceAlgorithm.java
@@ -0,0 +1,38 @@
+package com.asafov.naum;
+
+public class LevenshteinDistanceAlgorithm {
+
+    public static void main(String[] args) {
+
+        System.out.println(levenstein("w","word"));
+    }
+
+    public static int levenstein(String x,String y) {
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1]
+                                    + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+    private static int min(int n1, int n2, int n3) {
+        return Math.min(Math.min(n1, n2), n3);
+    }
+}
